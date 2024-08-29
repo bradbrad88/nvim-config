d3af9f0a767d485be57e5c0bb984b49f315a126a
Brad Teague <b_rad88@live.com>
2024-08-29 18:17:02 +0930

    Formatting
    Use conform.nvim instead of null_ls due to it being archived

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -7,11 +7,39 @@
 
 
 return {
-    "jose-elias-alvarez/null-ls.nvim",
-    event = "VeryLazy",
-    opts = function()
-        return require("config.format")
-    end
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
+		conform.setup({
+			formatters_by_ft = {
+				javascript = { "tailwindcss", "prettier" },
+				typescript = { "prettier" },
+				javascriptreact = { "tailwindcss", "prettier" },
+				typescriptreact = { "tailwindcss", "prettier" },
+				lua = { "stylua" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			},
+		})
+
+		vim.opt.tabstop = 4
+		vim.opt.shiftwidth = 4
+		vim.opt.expandtab = true
+
+		vim.keymap.set({ "n", "v" }, "<leader>f", function()
+			print("formatting")
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			})
+		end, { desc = "" })
+	end,
+	opts = function()
+		return require("config.format")
+	end,
 }
-
-
