7b2470e8551dc45d30f4c4583f9be329a285ef40
Brad Teague <b_rad88@live.com>
2024-09-14 18:12:39 +0930

    Feat: Treesitter text-object keymappings
    Introduce movement between functions, blocks and assignments

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,3 +1,5 @@
+local keymaps = require("config.keymap").telescope_keymaps
+
 return {
 	{
 		"vhyrro/luarocks.nvim",
@@ -16,105 +18,48 @@ return {
 	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
-        dependencies = {"nvim-treesitter/nvim-treesitter-textobjects"},
-		opts = {
-			ensure_installed = { "lua", "vim", "javascript", "typescript", "html", "css" },
-			sync_install = false,
-			auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = true },
-            textobjects = {
-                select = {
-                    enable = true,
-                    lookahead = true,
-                    keymaps = {
-                        ["af"] = "@function.outer",
-                        ["if"] = "@function.inner",
-                    }
-                }
-            }
-		},
+		dependencies = { "nvim-treesitter/nvim-treesitter-textobjects", "nvim-treesitter/playground" },
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = { "lua", "vim", "javascript", "typescript", "html", "css" },
+				ignore_install = {},
+				modules = {},
+				sync_install = false,
+				auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = true },
+				textobjects = {
+					select = {
+						enable = true,
+						lookahead = true,
+						keymaps = {
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+							["ab"] = "@block.outer",
+							["ib"] = "@block.innter",
+						},
+					},
+					move = {
+						enable = true,
+						set_jumps = true,
+						goto_next_start = {
+							[']"'] = "@string.outer",
+							["]f"] = "@function.outer",
+							["]F"] = "@function.inner",
+							["]b"] = "@block.inner",
+							["]ah"] = "@assignment.lhs",
+							["]al"] = "@assignment.rhs",
+						},
+						goto_previous_start = {
+							["[f"] = "@function.outer",
+							["[F"] = "@function.inner",
+							["[b"] = "@block.inner",
+							["[ah"] = "@assignment.lhs",
+							["[al"] = "@assignment.rhs",
+						},
+					},
+				},
+			})
+		end,
 	},
-	-- {
-	-- 	"nvim-treesitter/nvim-treesitter-textobjects",
-	-- 	opts = {
-	-- 		textobjects = {
-	-- 			select = {
-	-- 				keymaps = {
-	-- 					["af"] = "@function.outer",
-	-- 					["if"] = "@function.inner",
-	-- 				},
-	-- 			},
-	-- 		},
-	-- 	},
-	-- },
-	-- {
-	-- 	"VonHeikemen/lsp-zero.nvim",
-	-- 	lazy = true,
-	-- 	config = false,
-	-- },
-	-- {
-	-- 	"neovim/nvim-lspconfig",
-	-- 	cmd = "LspInfo",
-	-- 	event = { "BufReadPre", "BufNewFile" },
-	-- 	dependencies = {
-	-- 		{ "hrsh7th/cmp-nvim-lsp" },
-	-- 	},
-	-- 	config = function()
-	-- 		local lsp_zero = require("lsp-zero")
-	-- 		local lsp_attach = function(client, bufnr)
-	-- 			local opts = { buffer = bufnr }
-	--
-	-- 			vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
-	-- 		end
-	--
-	-- 		lsp_zero.extend_lspconfig({
-	-- 			sign_text = true,
-	-- 			lsp_attach = lsp_attach,
-	-- 			capabilities = require("cmp_nvim_lsp").default_capabilities()
-	-- 		})
-	-- 		require("lspconfig").tsserver.setup({})
-	-- 	end
-	-- },
-	-- { "hrsh7th/cmp-nvim-lsp" },
-	-- { "hrsh7th/nvim-cmp" },
-	--	{
-	--		"neovim/nvim-lspconfig",
-	--		dependencies = { "williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim" },
-	--		config = function()
-	--			require("mason").setup()
-	--			require("mason-lspconfig").setup()
-	--		end,
-	--	},
-	--	{
-	--		"hrsh7th/nvim-cmp",
-	--		dependencies = {
-	--			"hrsh7th/cmp-nvim-lsp",
-	--			"hrsh7th/cmp-buffer",
-	--			"hrsh7th/cmp-path",
-	--			"hrsh7th/cmp-cmdline",
-	--			"L3MON4D3/LuaSnip",
-	--		},
-	--		config = function()
-	--			local cmp = require("cmp")
-	--			cmp.setup({
-	--				snippet = {
-	--					expand = function(args)
-	--						require("luasnip").lsp_expand(args.body)
-	--					end,
-	--				},
-	--				sources = cmp.config.sources({
-	--					{ name = "nvim_lsp" },
-	--					{ name = "buffer" },
-	--					{ name = "path" },
-	--				}),
-	--			})
-	--		end,
-	--	},
-	-- {
-	-- 	"jose-elias-alvarez/null-ls.nvim",
-	-- 	config = function()
-	-- 		require("null-ls").setup()
-	-- 	end,
-	-- },
 }
