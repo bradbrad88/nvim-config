11f8d7546c78b46f7eec35736744e997269f2900
Brad Teague <b_rad88@live.com>
2024-09-14 18:10:20 +0930

    Feat: Remove Neogit plugin
    In favour of LazyGit.
    Also fix gitsigns config by ensuring setup function is run

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,28 +1,12 @@
 local keymaps = require("config.keymap").gitsigns_keymaps
 return {
-    {
-        "lewis6991/gitsigns.nvim",
-        opts = {
-            on_attach = function(bufnr)
-                print("on attach git signs")
-                vim.cmd("set statusline+=%{get(b:,'gitsigns_status','')}")
-            end,
-        },
-    },
-    {
-        "NeogitOrg/neogit",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "sindrets/diffview.nvim",
-            "nvim-telescope/telescope.nvim",
-        },
-    },
 	{
 		"lewis6991/gitsigns.nvim",
-		config = function()
+		opts = function()
 			local gitsigns = require("gitsigns")
 			vim.cmd("set statusline+=%{get(b:,'gitsigns_status','')}")
 			keymaps(gitsigns)
+			return {}
 		end,
 	},
 }
